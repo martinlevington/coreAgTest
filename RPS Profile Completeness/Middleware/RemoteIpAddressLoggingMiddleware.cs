83061130ea3ee294441be2995a9b4dc2803ef3ac
Martin Levington <martin.levington@gmail.com>
2018-08-27 14:52:27 +0100

    updates

--- RPS Profile Completeness/Middleware/RemoteIpAddressLoggingMiddleware.cs
+++ RPS Profile Completeness/Middleware/RemoteIpAddressLoggingMiddleware.cs
@@ -1,28 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Serilog.Context;
 
 namespace RPS.Presentation.Middleware
 {
-public class RemoteIpAddressLoggingMiddleware
-  {
-  private readonly RequestDelegate _next;
-
-  public RemoteIpAddressLoggingMiddleware(RequestDelegate next)
-  {
-    _next = next;
-  }
-
-    public async Task Invoke(HttpContext context)
+    public class RemoteIpAddressLoggingMiddleware
     {
+        private readonly RequestDelegate _next;
 
-      using (LogContext.PushProperty("Address", context.Connection.RemoteIpAddress))
-      {
-        await _next.Invoke(context);
-      }
+        public RemoteIpAddressLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            using (LogContext.PushProperty("Address", context.Connection.RemoteIpAddress))
+            {
+                await _next.Invoke(context);
+            }
+        }
     }
-  }
 }
